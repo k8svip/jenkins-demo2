11a72725d235497821ae09c18a9e90d2340dfbff
k8svip <k8svip@163.com>
2019-06-10 15:33:50 +0800

    add

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// Import the fmt for formatting strings
+// Import os so we can read environment variables from the system
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("I Love Jenkins")
+	fmt.Println("I Love Jenkins")
+	fmt.Println("I Love Jenkins")
+}
